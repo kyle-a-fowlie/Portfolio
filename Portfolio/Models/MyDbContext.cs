24339501847de95d16dbeea3914ad3a13b41b750
Kyle Fowlie <kyle.a.fowlie@gmail.com>
2015-09-10 16:13:15 -0400

    Initial site framework.
    The bare minimum is starting to come together.

---
+++ Portfolio/Models/MyDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Portfolio.Models
+{
+    public class MyDbContext : DbContext
+    {
+        
+        // Database constructor
+        public MyDbContext() : base("Portfolio")
+        {
+
+        }
+
+        //public DbSet<Project> Projects { get; set;  }
+
+    }
+}
